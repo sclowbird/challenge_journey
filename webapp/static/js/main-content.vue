77ec159c584fbba7f6515f8eb7e23a7f617ca29a
sclowbird <gregor.hlinka@gmail.com>
2019-02-27 18:37:43 +0100

    Changed input behaviour.

--- webapp/static/js/main-content.vue
+++ webapp/static/js/main-content.vue
@@ -3,13 +3,16 @@
         <h1> Challenge Journey</h1>
         <h2> How many brave souls participate? </h2>
 
-        <form id="signup-form" @submit.prevent="processForm" v-for="(item, index) in participants" :key="index" v-on:input="checkEmptyField">
-           <!-- <p> {{ index }} </p> -->
-            <input v-if="index == inputFields" type="text"  name="participants" class="input" v-bind:id="`${index}`" v-on:input="newPlayer">
-            <input v-else type="text"  name="participants" class="input" v-bind:id="`test-${index}`">
-            <!-- <input type="text" class="input" name="participants" v-model="participants.name" v-on:input="newPlayer"> -->
+        <form id="signup-form" @submit.prevent="processForm(item)" v-for="(item, index) in participants" :key="index" v-on:input="checkEmptyField">
+            <input v-if="index == inputFields" type="text" name="participants" class="input" v-bind:id="`${index}`" v-on:input="newPlayer" v-model="item.name"> 
+            <input v-else type="text"  name="participants" class="input" v-bind:id="`test-${index}`" v-model="item.name">
+            <!-- <input type="text" class="input" name="participants" v-model="participants.name" v-on:input="newPlayer">-->          
         </form>
-        <input type="submit" value="Submit">
+            <input type="submit" value="Submit">
+
+        <p> {{ participants }}</p>
+            
+        
 
             
             
@@ -22,6 +25,7 @@
     export default {
         name: "main-content",
         data: () => ({
+            output: [],
             //keeps track of the number of names currently entered
             editIndex: null,
             //represents the number of created input fields
@@ -33,19 +37,17 @@
         }),
 
         methods: {
-            processForm: function() {
-                console.log({ participants: this.participants });
-
-                          
+            processForm: function(item) {
+                //console.log({ participants: this.participants.name });  
+                //console.log(this.participants[0].name);       
             },
 
-            newPlayer: function() {       
+            newPlayer: function() { 
                 this.editIndex = this.participants.length - 1        
                 let inputId = document.getElementsByClassName("INPUT")[this.inputFields];
-
                 //third attribute of inputId is: v-bind:id="`${index}`"
-                let index = inputId.attributes[3].value;
-         
+                let index = inputId.attributes[3].value;      
+
                 if(index == this.editIndex) {
                     this.participants.push({ name: '' })
                     this.inputFields += 1
@@ -59,10 +61,25 @@
 
                 //check if input field is empty
                 if(secondToLastInput === "") {
-                    //console.log(`Field ${this.inputFields} is empty.`)
                     this.participants.splice(this.inputFields - 1, 1);
                     this.inputFields -=1;
                 } 
+
+                for(let i=0; i < (this.inputFields); i++) {
+                    let checkAllInputs = document.getElementsByClassName("INPUT")[i].value;
+                    
+                    //TODO: Check how to remove input field with correct id.
+                    // Object properties are correctly deleted, but input fields
+                    // are still not correctly processed.
+                    if(checkAllInputs === "") {
+                        this.participants.splice(i, 1);
+                        //console.log(`Spliced index: ${i-1}`);
+                        this.inputFields -=1;
+                        //console.log(`this.inputFields: ${this.inputFields}`);
+                    }
+                    
+                }
+
             }
 
             
